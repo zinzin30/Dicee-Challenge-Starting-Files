158ce83a55c0100955be226803c03c3dd66f8257
zinzin30 <zinzin30@gmail.com>
2022-09-04 22:32:09 +0700

    completed

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-console.log(randomNumber1);
+document
+  .querySelector(".dice .img1")
+  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document
+  .querySelector(".dice .img2")
+  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
+randomNumber1 === randomNumber2
+  ? (document.querySelector(".container h1").innerHTML = "Draw!")
+  : randomNumber1 > randomNumber2
+  ? (document.querySelector(".container h1").innerHTML = "🚩 Player1 Wins!")
+  : (document.querySelector(".container h1").innerHTML = "Player2 Wins! 🚩");
